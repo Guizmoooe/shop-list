c780a9787d049fbf841d1d53608918471ecdf420
Guizmoooe <gmarques@DESKTOP-52F605O.localdomain>
2020-10-15 22:52:16 +0200

    first commit on new branch

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,12 @@ const moment = require('moment');
 module.exports = {
     register: async function (req, res) {
         // Get body informations
-        const email = req.body.email;
-        const username = req.body.username;
-        const password = req.body.password;
+        const {
+            email,
+            username,
+            password
+        } = req.body;
+
         // Check parameters before control
         if (email == null || username == null || password == null) {
             return res.status(400).json({
@@ -19,7 +22,7 @@ module.exports = {
         }
         // Check if user exist in DB
         const userFound = await userModel.findOne({
-            email: email
+            email
         })
         if (!userFound) {
             //const bcryptedEmail = bcrypt.hashSync(email, 5) // TODO RGPD
@@ -40,11 +43,14 @@ module.exports = {
     },
     login: async function (req, res) {
         // Get body informations
-        const email = req.body.email;
-        const password = req.body.password;
+        const {
+            email,
+            password
+        } = req.body;
+
         // Check if user exist in DB and get associated response
         const userFound = await userModel.findOne({
-            email: email
+            email
         });
         userFound !== null ?
             bcrypt.compareSync(password, userFound.password) ?
@@ -52,9 +58,13 @@ module.exports = {
             res.status(403).send("Invalid login or password") :
             res.status(403).send("Invalid login or password")
     },
-    delete: function(req, res) {
-        const id = req.body.id;
-        userModel.deleteOne({_id: id}).then(() =>{
+    delete: function (req, res) {
+        const {
+            id
+        } = req.body;
+        userModel.deleteOne({
+            _id: id
+        }).then(() => {
             res.status(200).send("Delete with success")
         }).catch((err) => {
             res.status(409).send(err)
